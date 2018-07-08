1d9d9fb06aabfded7eee1e413b6e37e9e78497e3
RedTyke <john.mcniffe@hotmail.com>
2018-07-08 18:11:58 +0100

    Initial Commit

---
+++ MathsGridProgramatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathsGridProgramatically
+//
+//  Created by John McNiffe on 08/07/2018.
+//  Copyright © 2018 John McNiffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
